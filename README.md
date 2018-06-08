536a4e9548a2a3bc07d568a3869c30115cc18237
Dmitry Saltykov <d.saltykov@yandex.ru>
2018-06-08 09:43:22 +0800

    added save in html

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ Parser for web site https://www.memrise.com
 
 #### Usage
 
-1. Write in application.properties your memrise session id from cookie "sessionid_2" (auth from google), property - "memrise.session.id=_your_sessionid_".
-Write via a semicolon your memrise lessons urls in property "memrise.lesson.urls=".
+1. Write via a semicolon your memrise lessons urls in property "memrise.lesson.urls=".
 Write via a semicolon count parts in memrise lesson course in property "memrise.lesson.parts=".
 Write via a semicolon file name for memrise lesson course in property "memrise.lesson.titles=".
 1. ```mvn clean package```
--- src/main/java/memrise/parser/service/ParserService.java
+++ src/main/java/memrise/parser/service/ParserService.java
@@ -25,21 +25,21 @@ import java.util.List;
 public class ParserService {
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final RestTemplate restTemplate;
-    private final String sessionId;
     private final List<String> memriseLessonUrls;
     private final List<String> memriseLessonParts;
     private final List<String> memriseLessonTitles;
+    private final String memriseExportType;
 
     @Autowired
-    public ParserService(@Value("${memrise.session.id}") String sessionId,
-                         @Value("#{'${memrise.lesson.urls}'.split(';')}") List<String> memriseLessonUrls,
+    public ParserService(@Value("#{'${memrise.lesson.urls}'.split(';')}") List<String> memriseLessonUrls,
                          @Value("#{'${memrise.lesson.parts}'.split(';')}") List<String> memriseLessonParts,
-                         @Value("#{'${memrise.lesson.titles}'.split(';')}") List<String> memriseLessonTitles) {
+                         @Value("#{'${memrise.lesson.titles}'.split(';')}") List<String> memriseLessonTitles,
+                         @Value("${memrise.export.type}") String memriseExportType) {
         this.restTemplate = new RestTemplate();
-        this.sessionId = sessionId;
         this.memriseLessonUrls = memriseLessonUrls;
         this.memriseLessonParts = memriseLessonParts;
         this.memriseLessonTitles = memriseLessonTitles;
+        this.memriseExportType = memriseExportType;
     }
 
     @PostConstruct
@@ -47,7 +47,6 @@ public class ParserService {
         log.info("Start memrise parser");
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Cookie", "sessionid_2=" + sessionId);
         HttpEntity requestEntity = new HttpEntity(httpHeaders);
 
         for (int urlCounter = 0; urlCounter < memriseLessonUrls.size(); urlCounter++) {
@@ -60,20 +59,22 @@ public class ParserService {
                 String responseBody = (String) responseEntity.getBody();
                 Document doc = Jsoup.parse(responseBody);
 
+                String titleCourse = memriseLessonTitles.get(urlCounter);
+                String titleCoursePart = (partCounter < 9 ? "0" : "") + (partCounter + 1);
                 String title = doc.getElementsByClass("progress-box-title").text();
                 List<Element> elementsA = doc.getElementsByClass("col_a");
                 List<Element> elementsB = doc.getElementsByClass("col_b");
 
-                List<String> content = new ArrayList<>();
-                content.add("title\t" + title);
-                content.add("eng\trus");
-                for (int i = 0; i < elementsA.size(); i++) {
-                    content.add(elementsA.get(i).text() + "\t" + elementsB.get(i).text());
+                List<String> content;
+                if (memriseExportType.equals("csv")) {
+                    content = prepareCSVContent(title, elementsA, elementsB);
+                } else {
+                    content = prepareHTMLContent(titleCourse, titleCoursePart, title, elementsA, elementsB);
                 }
 
                 PrintWriter writer;
                 try {
-                    writer = new PrintWriter(memriseLessonTitles.get(urlCounter) + "_" + (partCounter < 9 ? "0" : "") + (partCounter + 1) + ".csv", "UTF-8");
+                    writer = new PrintWriter(titleCourse + "_" + titleCoursePart + "." + memriseExportType, "UTF-8");
                     for (String str : content) {
                         writer.println(str);
                     }
@@ -85,5 +86,29 @@ public class ParserService {
         }
 
         log.info("Finished memrise parser");
+    }
+
+    private List<String> prepareCSVContent(String title, List<Element> elementsA, List<Element> elementsB) {
+        List<String> content = new ArrayList<>();
+        content.add("title\t" + title);
+        content.add("eng\trus");
+        for (int i = 0; i < elementsA.size(); i++) {
+            content.add(elementsA.get(i).text() + "\t" + elementsB.get(i).text());
+        }
+        return content;
+    }
+
+    private List<String> prepareHTMLContent(String titleCourse, String titleCoursePart, String title, List<Element> elementsA, List<Element> elementsB) {
+        List<String> content = new ArrayList<>();
+        content.add("<!DOCTYPE html><html><body><table border=\"1\" width=\"100%\" style=\"font-size:24px;\">");
+        content.add("<caption><h2>" + titleCourse + " - part [" + titleCoursePart + "] - " + title + "</h2></caption>");
+        content.add("<tr><th>ENG</th><th>RUS</th></tr>");
+        for (int i = 0; i < elementsA.size(); i++) {
+            String en = elementsA.get(i).text();
+            String ru = elementsB.get(i).text();
+            content.add("<tr><td width=\"50%\">" + en + "</td><td width=\"50%\">" + ru + "</td></tr>");
+        }
+        content.add("</table></body></html>");
+        return content;
     }
 }
